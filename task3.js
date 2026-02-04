838891cbc2273583ad51d4c692ab06d82d719e38
mehedi hasan <mehedihasan.j135@gmail.com>
2026-02-04 14:28:51 +0600

    task3 complete

---
+++ task3.js
@@ -0,0 +1,25 @@
+// Task-3
+// Display the physics marks as output.
+
+// const student = {
+//     name: "Hamim Sakep",
+//     id: 5421,
+//     physics: {
+//         subject: "HSC Physics",
+//         author: "Shahjahan Tapan",
+//         marks: 30
+//     }
+// };
+
+const student = {
+  name: "Hamim Sakep",
+  id: 5421,
+  physics: {
+    subject: "HSC Physics",
+    author: "Shahjahan Tapan",
+    marks: 30,
+  },
+};
+
+const physicsResult = student.physics.marks;
+console.log(physicsResult);
